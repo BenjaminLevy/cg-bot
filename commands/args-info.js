8b62d6738ab5d99a0376d9ba8df613d01f0670c6
Benjamin <BnLvy1@gmail.com>
2021-01-31 17:33:34 -0800

    will add Sheets integration after this

---
+++ commands/args-info.js
@@ -0,0 +1,31 @@
+module.exports = {
+		name: 'args-info',
+	description: 'returns username and user ID',
+	args: true,
+	execute(message, args) {
+ 			if (args[0] === 'foo') {
+				return message.channel.send('bar');
+			}
+
+			message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
+	},
+};
+
+//
+// 	message.channel.send(`first argument ${args[0]}`);
+// }
+// else if (command === 'avatar') {
+// 	if (!message.mentions.users.size) {
+// 		return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+// 	}
+// 	const avatarList = message.mentions.users.map(user => {
+// 	return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+// 	});
+// 	console.log(avatarList);
+//
+// 	// send the entire array of strings as a message
+// 	// by default, discord.js will `.join()` the array with `\n`
+// 	message.channel.send(avatarList);
+//
+// // ...
+// }
--- index.js
+++ index.js
@@ -1,59 +1,50 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on(
-  'message', message => { 
-
+  'message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(' ');
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-  if (command === (`ping`)) {
-  	// send back "Pong." to the channel the message was sent in
-  	message.channel.send('pong');
-  }
-  else if (command === `bleep`) {
-    message.channel.send('bloop')
+	if (!client.commands.has(commandName)) return;
 
-  }
-  else if (command === `user-info`) {
-	message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-  }
-	else if (command === 'args-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		else if (args[0] === 'foo') {
-			return message.channel.send('bar');
-		}
+	const command = client.commands.get(commandName);
 
-		message.channel.send(`first argument ${args[0]}`);
+	if (command.args && !args.length) {
+	return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
-	else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
-		}
-		const avatarList = message.mentions.users.map(user => {
-		return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
-		});
-		console.log(avatarList);
 
-		// send the entire array of strings as a message
-		// by default, discord.js will `.join()` the array with `\n`
-		message.channel.send(avatarList);
-
-	// ...
+	try {
+	command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 
 
-});
 
+	});
 
 
 client.login(token);
