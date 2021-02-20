eb1459ad44a93349b9d9b669f98ce8ddd64b978a
Benjamin <BnLvy1@gmail.com>
2021-02-19 21:38:10 -0800

    scihub command fully functioning

--- commands/scihub.js
+++ commands/scihub.js
@@ -4,12 +4,44 @@ module.exports = {
 	description: 'takes pubmed link and returns scihub link',
 	args: true,
 	execute(message, args) {
-		let inputedID = args[0]
-		let returnedObject = `https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids=${inputedID}&format=json`
+	const fetch = require("../node_modules/node-fetch");
+	// const urlPubmed = new URL(args[0]);
+	// let inputID = urlPubmed.pathname.slice(1, -1)
 
-		message.channel.send();
+	const inputID = args[0]
+
+  const url = args[0]
+	console.log(url);
+		fetch(url)
+      .then(res => res.text()) // parse response as JSON
+      .then(html => {
+				let doiPosition = html.indexOf('doi');
+				let doiEnd = (html.length - doiPosition) * (-1)
+				console.log(doiPosition);
+				let doiRaw = html.slice(doiPosition + 4, doiPosition + 100)
+				console.log(doiRaw);
+				let commaPosition = doiRaw.indexOf(',')
+				let doi = doiRaw.slice(0, commaPosition)
+				// const urlPubmed = new URL(args[0]);
+				// let urlNoSlashes = urlPubmed.pathname.slice(1, -1)
+				// console.log(`https://sci-hub.do/${urlNoSlashes}`);
+				// let doi = (data.records[0].doi);
+       message.channel.send(`https://sci-hub.do/${doi}`);
+
+
+			})
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
 	},
 };
+
+// <span class="citation-doi">
+// 			 doi: 10.1016/j.sleep.2018.09.008.
+// 		 </span>
+
+ // <span class="docsum-journal-citation full-journal-citation">Otolaryngol Head Neck Surg. 2015 Sep;153(3):326-33. doi: 10.1177/0194599815594374. Epub 2015 Jul 16.</span>
 
 //
 // 	message.channel.send(`first argument ${args[0]}`);
