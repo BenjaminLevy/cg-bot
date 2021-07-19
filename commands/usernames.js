fed898961518c4070c0199dc562d99e3f8465469
Benjamin <BnLvy1@gmail.com>
2021-07-18 17:00:15 -0700

    usernames

--- commands/usernames.js
+++ commands/usernames.js
@@ -1,7 +1,7 @@
 const { Channel } = require("discord.js");
 
 const flatCache = require('flat-cache');
-const cache = flatCache.load('userCache');
+const cache = flatCache.load('userCache8');
 
 module.exports = {
 	name: 'usernames',
