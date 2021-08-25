1f391bbf8d5fb24d004101919309f098875f91c4
Benjamin <BnLvy1@gmail.com>
2021-08-25 16:04:51 -0700

    clean up lit.js

--- commands/lit.js
+++ commands/lit.js
@@ -24,14 +24,9 @@ module.exports = {
 
 		  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
 		  const rows = await sheet.getRows();
-		  const c6 = sheet.getCellByA1
-
-		  // console.log(sheet.title);
-		  // console.log(sheet.rowCount);
 
 		  await sheet.loadCells('A1:C65'); // loads a range of cells
 		  console.log(sheet.cellStats); // total cells, loaded, how many non-empty
-		  const a1 = sheet.getCell(1, 0); // access cells using a zero-based index
 		  console.log(sheet.getCell(1, 0).value);
 
 			if(keyword == "help"){
@@ -63,5 +58,3 @@ module.exports = {
 	},
 };
 
-
-// accesssSpreadsheet();
