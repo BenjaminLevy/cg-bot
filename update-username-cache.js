d18a4f4b15af72ee6f25aa6aba085f603243108a
Benjamin <BnLvy1@gmail.com>
2021-09-04 19:51:59 -0700

    seperate update username cache into two functions

--- update-username-cache.js
+++ update-username-cache.js
@@ -2,9 +2,14 @@ var flatCache = require('flat-cache');
 const cache = flatCache.load('freshCache02');
 const { userHistoryChannelID, serverID } = require('./config.json')
 
+//TODO add error handling
 
+module.exports = function (client, specificUserID = undefined){
+  if(!specificUserID) { checkAllUsers(client) };
+  else { updateSpecificUser(client, specificUserID) }
+}
 
-module.exports = async function (client){
+async function checkAllUsers(client){
 
  let guild = client.guilds.cache.get(serverID);
  let membersMap = await guild.members.fetch()
@@ -32,10 +37,43 @@ module.exports = async function (client){
       cache.setKey(id, cachedUsernameArray);
       console.log(`CHANGED username: "${cachedUsernameArray[1]}" is now ${cachedUsernameArray[0]}`)
     }
+    else(console.log(`not changed: ${cachedUsernameArray[0]}`))
   }
   cache.save();
+
 }
 
-function checkAllUsers(){
+async function updateSpecificUser(client, specificUserID){
+
+ let guild = client.guilds.cache.get(serverID);
+ let id = await guild.members.fetch(specificUserID)
+ 
+console.log(id) 
+  // for (const [key,value] of membersMap) {
+
+  //   let username = membersMap.get(key).user.username;
+  //   let id = membersMap.get(key).user.id;
   
+  //   //Returns array of usernames
+  //   let cachedUsernameArray = cache.getKey(id);
+
+  //   //Sets cachedUsedname to most recent username in cachedUsernameArray
+  //   // let cachedUsername = cachedUsernameArray[0]
+
+  //   /* If id is not in cache, insert into cache with
+  //   the id as key and array with username as value */
+  //   if(cachedUsernameArray === undefined){
+  //     let usernameArray = [username]
+  //     cache.setKey(id, usernameArray)
+  //     console.log(`NEW username: ${username}`) 
+  //   }
+  //   else if(cachedUsernameArray[0] != username){
+  //     cachedUsernameArray.unshift(username);
+  //     cache.setKey(id, cachedUsernameArray);
+  //     console.log(`CHANGED username: "${cachedUsernameArray[1]}" is now ${cachedUsernameArray[0]}`)
+  //   }
+  //   else(console.log(`not changed: ${cachedUsernameArray[0]}`))
+  // }
+  // cache.save();
+
 }
