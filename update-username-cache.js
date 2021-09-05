e05feadba6563467a48ba3181cdd5b27277b350f
Benjamin <BnLvy1@gmail.com>
2021-09-04 17:44:49 -0700

    fix username tracking

--- update-username-cache.js
+++ update-username-cache.js
@@ -1,4 +1,4 @@
-const flatCache = require('flat-cache');
+var flatCache = require('flat-cache');
 const cache = flatCache.load('freshCache02');
 const { userHistoryChannelID, serverID } = require('./config.json')
 
@@ -6,87 +6,36 @@ const { userHistoryChannelID, serverID } = require('./config.json')
 
 module.exports = async function (client){
 
-    let guild = client.guilds.cache.get(serverID);
-	// let str = "Members:\n";
+ let guild = client.guilds.cache.get(serverID);
+ let membersMap = await guild.members.fetch()
 
-	// let x = guild.members.cache
-  //  .each(m => {
-  //    str += `\nName: ${m.user.username} ID: ${m.id}`
+  for (const [key,value] of membersMap) {
 
-  //  });
-  //  console.log( str );
- let members = await guild.members.fetch()
-  console.log(members)
-}
-/* In the cache each value is an array named "usernameArray".
-The most recent username is held at the 0th index. */
-function checkUsernameAgainstCache(value, key, map){
+    let username = membersMap.get(key).user.username;
+    let id = membersMap.get(key).user.id;
   
-let username = map.get(key).user.username;
-let id = map.get(key).user.id;
-console.log(id + '...' + username)
-//Returns array of usernames
-let cachedUsernameArray = cache.getKey(id);
-console.log(cachedUsernameArray)
-console.log(cache.all())
+    //Returns array of usernames
+    let cachedUsernameArray = cache.getKey(id);
 
-//Sets cachedUsedname to most recent username in cachedUsernameArray
-// let cachedUsername = cachedUsernameArray[0]
+    //Sets cachedUsedname to most recent username in cachedUsernameArray
+    // let cachedUsername = cachedUsernameArray[0]
 
-/* If id is not in cache, insert into cache with
- the id as key and array with username as value */
-if(cachedUsernameArray === undefined){
-  let usernameArray = [username]
-  cache.setKey(id, usernameArray)
-  console.log(`NEW username: ${username}`)
-  cache.save()
-  console.log(cache.all())
+    /* If id is not in cache, insert into cache with
+    the id as key and array with username as value */
+    if(cachedUsernameArray === undefined){
+      let usernameArray = [username]
+      cache.setKey(id, usernameArray)
+      console.log(`NEW username: ${username}`) 
+    }
+    else if(cachedUsernameArray[0] != username){
+      cachedUsernameArray.unshift(username);
+      cache.setKey(id, cachedUsernameArray);
+      console.log(`CHANGED username: "${cachedUsernameArray[1]}" is now ${cachedUsernameArray[0]}`)
+    }
+  }
+  cache.save();
 }
-else if(cachedUsernameArray[0] != username){
-  cachedUsernameArray.unshift(username);
-  cache.setKey(id, cachedUsernameArray);
-  console.log(`CHANGED username: ${cachedUsernameArray[1]} is now ${cachedUsernameArray[0]}`)
-  cache.save()
 
+function checkAllUsers(){
+  
 }
-// console.log(`Username: ${username} Id: ${id}`)
-
-}
-// module.exports = function (client){
-//     let guild = client.guilds.cache.get("731543013846941736");
-// 	try{
-// 	guild.members.cache.forEach(function (m){
-// 		let oldUsernames = cache.getKey(m.user.id)
-// 		// console.log(oldUsernames)
-// 		if(cache.getKey(m.user.id) === undefined){
-			
-// 			cache.setKey(m.user.id, [{user: m.user.username, date: Date()}])
-// 			let newUsername = cache.getKey(m.user.id)[0].user
-// 			console.log(`NEW...${newUsername}`);
-			
-// 		}
-// 		else if(oldUsernames[0].user !== m.user.username){
-// 			oldUsernames.unshift({user: m.user.username, date: Date()})
-// 			let previousUsername = cache.getKey(m.user.id)[1].user
-// 			cache.setKey(m.user.id, oldUsernames)
-// 			let newUsername = cache.getKey(m.user.id)[0].user
-// 			console.log(`CHANGED...${newUsername}`);
-// 			const channel = guild.channels.cache.get(userHistoryChannelID);
-//   			channel.send(`CHANGED: ${previousUsername} is now ${newUsername}`);
-// 		}
-// 		else{
-// 			let allNames = cache.getKey(m.user.id);
-// 			console.log("username cache else")
-// 			for(let i = 0; i < allNames.length; i++){
-// 				console.log(allNames[i].user);
-// 			}
-// 			console.log(`old...${oldUsernames[0].user}`);
-// 		}
-// 	})
-// 	console.log('Cache complete');
-// 	// console.log(cache.all())
-// 	cache.save()
-// 	} catch (error) {
-// 		console.error(error);
-//     }
-// }
